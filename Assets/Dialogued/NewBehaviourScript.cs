79db2b7ddde7c5f6238ab42130d55d5868f27a36
Egor Martynov <43707519+martingor@users.noreply.github.com>
2021-05-25 23:12:19 +0300

    Fixed bunch of stuff

---
+++ Assets/Dialogued/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void ClickToOpenClose()
+    {
+        if (this.gameObject.activeSelf) // проверка активно ли окно
+        {
+            this.gameObject.SetActive(false); // закрываем если активно
+        }
+        else this.gameObject.SetActive(true);
+    }
+}
